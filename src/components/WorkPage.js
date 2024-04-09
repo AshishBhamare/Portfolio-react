444876a9907c276cac4651a2f64d332eed8d4401
Ashish Bhamare <87072128+AshishBhamare@users.noreply.github.com>
2024-04-09 14:13:05 +0530

    ui changes

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -15,7 +15,7 @@ import BigTitlte from "../subComponents/BigTitlte";
 const Box = styled.div`
   background-color: ${(props) => props.theme.body};
 
-  height: 700vh;
+  height: 750vh;
   position: relative;
   display: flex;
   align-items: center;
--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,34 +3,25 @@
 export const Work = [
     {
         id: 1,
-        name: "2FA app dashboard",
-        description: "Revamped the complete UX and UI of Two factor authentication app added multiple features such as email and whatsapp integration for sending OTP, custom template support, popup UI gallery, country wise pricing plan etc",
-        tags: ["XD", "JSP", "JAVA"],
+        name: "My Figma designs",
+        description: "Some personal designs created with Figma. From sleek interfaces to engaging user experiences",
+        tags: ["Figma"],
 
-        demo: "https://apps.shopify.com/2-factor-authentication-and-passwordless-login",
-        // github:"https://github.com/codebucks27/Agency-website"
+        demo: "https://www.figma.com/proto/nv3lQMPFzrwa950ncfu87J/Sample-Screens?page-id=0%3A1&type=design&node-id=1-270&viewport=-2088%2C733%2C0.4&t=nbFgo1Jf1YPOs66U-1&scaling=min-zoom&mode=design",
+        // github:""
+
     },
     {
         id: 2,
-        name: "NFT marketplace application",
-        description: "Design and devloped NFT marketplace application from scratch which can convert your Shopify store into NFT marketplace solution like Opensea",
-        tags: ["Adobe XD", "React", "Blockchain", "JAVA"],
-
-        demo: "https://apps.shopify.com/nft-minting-and-marketplace",
-        // github:"https://github.com/codebucks27/react-job-listing-website"
-
-    },
-    {
-        id: 3,
-        name: "Single Sign On application UI revamped",
-        description: "Revamped the UI of this application and migrated it to React.js using Remix and Shopify's design system 'Polaris' you can see Figma protype on below link",
+        name: "SaaS Product - Build your own NFT Marketplace",
+        description: "NFT Marketplace is an online platform that enables users to buy NFTs and sell NFTs. It supports several blockchains and multiple crypto wallet connection",
         tags: ["Figma", "React", "Remix", "UI", "UX"],
 
-        demo: "https://www.figma.com/proto/wgy4b5HncnLuO8vi9TZVOc/Dashcube---Dashboard-UI-Builder-(Community)?page-id=99%3A3073&node-id=301-1657&scaling=min-zoom&starting-point-node-id=99%3A3120&mode=design&t=64q9GU8wVqAhZKzw-1",
+        demo: "https://hascoin.io/",
         // github:"https://github.com/codebucks27/devto-clone"
     },
     {
-        id: 4,
+        id: 3,
         name: "SaaS LMS application",
         description: "Designed complete new design system for SaaS based Learning Management System on Adobe XD and later on devloped it using React and next.js",
         tags: ["AdobeXD", "next", "React"],
@@ -40,7 +31,27 @@ export const Work = [
 
     },
     {
+        id: 4,
+        name: "Single Sign On application UI revamped",
+        description: "Revamped the UI of this application and migrated it to React.js using Remix and Shopify's design system 'Polaris' you can see Figma protype on below link, Click on the buttons to enjoy the prototype",
+        tags: ["Figma", "React", "Remix", "UI", "UX"],
+
+        demo: "https://www.figma.com/proto/wgy4b5HncnLuO8vi9TZVOc/Dashcube---Dashboard-UI-Builder-(Community)?page-id=99%3A3073&node-id=329-1090&scaling=min-zoom&starting-point-node-id=99%3A3120&t=64q9GU8wVqAhZKzw-1",
+        // github:"https://github.com/codebucks27/devto-clone"
+    },
+    {
         id: 5,
+        name: "API security Webpage design",
+        description: "Designed landing page for API security using Figma",
+        tags: ["Figma"],
+
+        demo: "https://www.figma.com/proto/qrKKYqhlKCRBIeyMGLUm2o/Free-landing-page---Qubly-(Community)?page-id=0%3A1&type=design&node-id=3-99&viewport=-49%2C303%2C0.23&t=F7YdUyf58QyoNLXs-1&scaling=min-zoom",
+        github: ""
+
+    },
+
+    {
+        id: 6,
         name: "Streaming channel design and devlopment",
         description: "This Roku channel delivers a rich learning experience to endusers, specially crafted to meet the client's specifications and prime user experience.",
         tags: ["AdobeXD", "Brightscript", "XML"],
@@ -50,25 +61,26 @@ export const Work = [
 
     },
     {
-        id: 6,
-        name: "Shopify Dashboard clone",
-        description: "Developed a replica of the Shopify Admin Dashboard using Node.js. Offering a comprehensive solution that mirrors the functionality and aesthetics of the original Shopify Admin Dashboard to increse the staff members for the client requirements using Shopify api's",
-        tags: ["node", "RestAPI"],
-        github: ""
-
-    },
-    {
         id: 7,
-        name: "API security Webpage design",
-        description: "Designed landing page for API security using Figma",
-        tags: ["Figma"],
+        name: "NFT marketplace application",
+        description: "Design and devloped NFT marketplace application from scratch which can convert your Shopify store into NFT marketplace solution like Opensea",
+        tags: ["Adobe XD", "React", "Blockchain", "JAVA"],
 
-        demo: "https://www.figma.com/proto/qrKKYqhlKCRBIeyMGLUm2o/Free-landing-page---Qubly-(Community)?page-id=0%3A1&type=design&node-id=3-99&viewport=-49%2C303%2C0.23&t=F7YdUyf58QyoNLXs-1&scaling=min-zoom",
-        github: ""
+        demo: "https://apps.shopify.com/nft-minting-and-marketplace",
+        // github:"https://github.com/codebucks27/react-job-listing-website"
 
     },
     {
         id: 8,
+        name: "2FA app dashboard",
+        description: "Revamped the complete UX and UI of Two factor authentication app added multiple features such as email and whatsapp integration for sending OTP, custom template support, popup UI gallery, country wise pricing plan etc",
+        tags: ["XD", "JSP", "JAVA"],
+
+        demo: "https://apps.shopify.com/2-factor-authentication-and-passwordless-login",
+        // github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id: 9,
         name: "Home Decor Web Page Design",
         description: "Designed landing page for Home texa webite",
         tags: ["AdobeXD"],
@@ -78,7 +90,7 @@ export const Work = [
 
     },
     {
-        id: 9,
+        id: 10,
         name: "And many more...",
         description: "",
         tags: [""],
